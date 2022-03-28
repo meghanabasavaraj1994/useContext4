ce724ecf31f23f6f7e7cd35cbad2c14333e60eeb
meghanabasavaraj1994 <102538632+meghanabasavaraj1994@users.noreply.github.com>
2022-03-29 01:12:14 +0530

    Update AuthContext.js

--- AuthContext.js
+++ AuthContext.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// Creating the context object and passing the default values.
 const authContext = React.createContext({status:null,login:()=>{}});
 
 export default authContext;
